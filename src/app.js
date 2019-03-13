7ae11f82914cf4c960c70771b6496909e7860a75
rudy <unixc3t@gmail.com>
2019-03-13 14:52:39 +0800

    add upload file size

--- src/app.js
+++ src/app.js
@@ -24,7 +24,7 @@ app.configure(configuration());
 app.use(helmet());
 app.use(cors());
 app.use(compress());
-app.use(express.json());
+app.use(express.json({limit:'10mb'}));
 app.use(express.urlencoded({ extended: true }));
 app.use(favicon(path.join(app.get('public'), 'favicon.ico')));
 // Host the public folder
