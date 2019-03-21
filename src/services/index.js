c3505b7db993f44211a033b981cb06318d915cf0
rudy <unixc3t@gmail.com>
2019-03-21 13:07:30 +0800

    add user service

--- src/services/index.js
+++ src/services/index.js
@@ -1,5 +1,7 @@
 const recipes = require('./recipe/recipe.service.js');
+const users = require('./user/user.service.js');
 // eslint-disable-next-line no-unused-vars
 module.exports = function (app) {
   app.configure(recipes);
+  app.configure(users);
 };
